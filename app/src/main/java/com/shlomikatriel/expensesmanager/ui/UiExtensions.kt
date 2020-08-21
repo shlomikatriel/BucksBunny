d02c41f976fb55d88c4e6309f7dcf6a8e238a4d9
shlomikatriel <shlomikatriel@gmail.com>
2020-08-22 00:15:21 +0300

    replace the add button with material fab Closes #23

--- app/src/main/java/com/shlomikatriel/expensesmanager/ui/UiExtensions.kt
+++ app/src/main/java/com/shlomikatriel/expensesmanager/ui/UiExtensions.kt
@@ -1,5 +1,8 @@
 package com.shlomikatriel.expensesmanager.ui
 
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.ScaleAnimation
 import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import com.shlomikatriel.expensesmanager.logs.Logger
@@ -13,4 +16,22 @@ fun Fragment.configureToolbar(
         title,
         navigateUpEnabled
     ) ?: Logger.e("Main activity is null for fragment '${javaClass.simpleName}'")
+}
+
+fun View.startPopAnimation() {
+    val animation = ScaleAnimation(
+        1f,
+        1.4f,
+        1f,
+        1.4f,
+        Animation.RELATIVE_TO_SELF,
+        0.5f,
+        Animation.RELATIVE_TO_SELF,
+        0.5f
+    ).apply {
+        duration = 150
+        repeatCount = 1
+        repeatMode = Animation.REVERSE
+    }
+    startAnimation(animation)
 }
