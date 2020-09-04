08c609380b7008c3790dd7c0a6a6498c05242c82
shlomikatriel <shlomikatriel@gmail.com>
2020-09-04 12:47:17 +0300

    added exporting of database schemas to help with migrations

--- app/src/main/java/com/shlomikatriel/expensesmanager/database/AppDatabase.kt
+++ app/src/main/java/com/shlomikatriel/expensesmanager/database/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.shlomikatriel.expensesmanager.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Expense::class], version = 1, exportSchema = false)
+@Database(entities = [Expense::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
 
     companion object {
